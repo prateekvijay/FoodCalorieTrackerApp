2d9ae9ea235829ca38ce2af01ce64bd21a21d42f
Prateek Vijay <prateek.vijay@ril.com>
2023-09-09 18:12:34 +0530

    Multi module gradle setup

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    implementation("com.android.tools.build:gradle:8.1.1")
+}
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "18"
+}
