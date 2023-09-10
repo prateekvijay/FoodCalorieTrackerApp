dbd7f1bd3834304cec98378fbb23ee8825bf2fa3
Prateek Vijay <prateek.vijay@ril.com>
2023-09-10 14:52:07 +0530

    Onboarding screen UI

---
+++ app/src/main/java/com/prateek/foodcalorietrackerapp/FoodCalorieTrackerApp.kt
@@ -0,0 +1,8 @@
+package com.prateek.foodcalorietrackerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodCalorieTrackerApp : Application() {
+}
